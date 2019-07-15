916edfeba36924da8ac13b6b9a1153fbcad00940
meedipak <dipak.salve@gmail.com>
2019-07-15 23:44:45 +0530

    Add project files.

---
+++ WebApiLearning/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiLearning.Models;
+
+namespace WebApiLearning.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] Products = new Product[]
+        {
+           new Product { Id=1, Name = "Playing Car", Category="Toys", Price = 10  },
+           new Product { Id=2, Name = "Hammer", Category="Hardware", Price = 20 }
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return this.Products;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = this.Products.FirstOrDefault((p) => p.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
---
+++ WebApiLearning/Controllers/ValueController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+using WebApiLearning.Models;
+
+namespace WebApiLearning.Controllers
+{
+    public class ValueController : ApiController
+    {
+        Product[] Products = new Product[]
+        {
+           new Product { Id=1, Name = "Playing Car", Category="Toys", Price = 10  },
+           new Product { Id=2, Name = "Hammer", Category="Hardware", Price = 20 }
+        };
+
+        public void Post()
+        {
+
+        }
+
+        public HttpResponseMessage Get()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, Products);
+            
+            return response;
+        }
+    }
+}
